ee8a47ff2442d474d3bb6bf6cddd0704d180b8e4
Terry Brown <terrynbrown@gmail.com>
2020-12-19 21:22:50 -0600

    Move test source into repo. change paths

--- dml/tests/round_trip_comparison.sh
+++ dml/tests/round_trip_comparison.sh
@@ -1,15 +1,15 @@
 set -e
-SOURCE=/home/tbrown/t/Proj/nrri/GLRI/GLEI2/siteweb/schema/fish.g2.dml
+SOURCE=fish.g2.dml
 cp "$SOURCE" workdir/orig.dml
-python dmlio.py --mode dml workdir/orig.dml >workdir/identity.dml
-python dmlio.py --mode yaml workdir/orig.dml >workdir/yaml.yaml
-python dmlio.py --mode json workdir/orig.dml >workdir/json.json
-python dmlio.py --mode dml workdir/yaml.yaml >workdir/yaml.dml
-python dmlio.py --mode dml workdir/json.json >workdir/json.dml
-python dotgraph.py workdir/orig.dml workdir/orig.pdf
-python dotgraph.py workdir/identity.dml workdir/identity.pdf
-python dotgraph.py workdir/yaml.dml workdir/yaml.pdf
-python dotgraph.py workdir/json.dml workdir/json.pdf
+python ../dmlio.py --mode dml workdir/orig.dml >workdir/identity.dml
+python ../dmlio.py --mode yaml workdir/orig.dml >workdir/yaml.yaml
+python ../dmlio.py --mode json workdir/orig.dml >workdir/json.json
+python ../dmlio.py --mode dml workdir/yaml.yaml >workdir/yaml.dml
+python ../dmlio.py --mode dml workdir/json.json >workdir/json.dml
+python ../dotgraph.py workdir/orig.dml workdir/orig.pdf
+python ../dotgraph.py workdir/identity.dml workdir/identity.pdf
+python ../dotgraph.py workdir/yaml.dml workdir/yaml.pdf
+python ../dotgraph.py workdir/json.dml workdir/json.pdf
 echo evince workdir/orig.pdf workdir/identity.pdf \
             workdir/yaml.pdf workdir/json.pdf
 
